03e430a19a815d08a13c0c64894d33f9892f8a7a
SuzukiHayato0901 <lingmusadou912@gmail.com>
2026-06-11 15:43:40 +0900

    重力反転（戻すことできない）

--- Gravity/Assets/Scripts/Gimmicks/GravityController.cs
+++ Gravity/Assets/Scripts/Gimmicks/GravityController.cs
@@ -1,14 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class GravityController : MonoBehaviour
 {
+    public Vector3 localGravity;
+
+    [SerializeField] private float rbSpeed;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        setLocalGravity();
+    }
+
+    void setLocalGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rb.AddForce(localGravity, ForceMode.Acceleration);
+
+            if(rb.useGravity == false)
+                rb.useGravity = true;
+             else
+                rb.useGravity = false;
+        }
     }
 }
